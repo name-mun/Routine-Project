605a47e743cb867eed3b89b867c543a81b4b491b
name-mun <tains9153@gmail.com>
2025-01-04 14:35:40 +0900

    ♻️ BoardColor 수정

--- Routine/Routine/Models/RoutineData/BoardColor.swift
+++ Routine/Routine/Models/RoutineData/BoardColor.swift
@@ -10,45 +10,94 @@ import UIKit
 ///보드 컬러 열거형
 ///
 ///uiColor() 메서드를 통해 UIColor를 반환
-///
-enum BoardColor: Codable, CaseIterable, Comparable {
-    
+
+enum BoardColor: Int, Codable, CaseIterable {
+
     case white
-    case yello
-    case blue
-    case green
-    case red
+    case pastelYellow
+    case pastelPink
+    case pastelPeach
+    case ivory
+    case creamyBeige
+    case softIvory
+    case mintGreen
+    case pastelGreen
+    case lightAqua
+    case softAqua
+    case lilac
 
     func uiColor() -> UIColor {
         switch self {
         case .white:
-            return UIColor(red: cgFloat(200),
-                           green: cgFloat(150),
-                           blue: cgFloat(200),
-                           alpha: 1)
-            
-        case .yello:
-            return UIColor(red: cgFloat(150),
-                           green: cgFloat(200),
+            return UIColor(red: cgFloat(255),
+                           green: cgFloat(255),
                            blue: cgFloat(255),
                            alpha: 1)
-            
-        case .blue:
-            return UIColor(red: cgFloat(130),
-                           green: cgFloat(170),
-                           blue: cgFloat(200),
+
+        case .pastelYellow:
+            return UIColor(red: cgFloat(244),
+                           green: cgFloat(244),
+                           blue: cgFloat(245),
                            alpha: 1)
-            
-        case .green:
-            return UIColor(red: cgFloat(170),
-                           green: cgFloat(200),
+
+        case .pastelPink:
+            return UIColor(red: cgFloat(254),
+                           green: cgFloat(243),
+                           blue: cgFloat(245),
+                           alpha: 1)
+
+        case .pastelPeach:
+            return UIColor(red: cgFloat(253),
+                           green: cgFloat(247),
+                           blue: cgFloat(243),
+                           alpha: 1)
+
+        case .ivory:
+            return UIColor(red: cgFloat(251),
+                           green: cgFloat(251),
+                           blue: cgFloat(241),
+                           alpha: 1)
+
+        case .creamyBeige:
+            return UIColor(red: cgFloat(251),
+                           green: cgFloat(247),
+                           blue: cgFloat(236),
+                           alpha: 1)
+
+        case .softIvory:
+            return UIColor(red: cgFloat(251),
+                           green: cgFloat(252),
+                           blue: cgFloat(241),
+                           alpha: 1)
+
+        case .mintGreen:
+            return UIColor(red: cgFloat(244),
+                           green: cgFloat(250),
+                           blue: cgFloat(236),
+                           alpha: 1)
+
+        case .pastelGreen:
+            return UIColor(red: cgFloat(241),
+                           green: cgFloat(251),
+                           blue: cgFloat(247),
+                           alpha: 1)
+
+        case .lightAqua:
+            return UIColor(red: cgFloat(241),
+                           green: cgFloat(252),
+                           blue: cgFloat(251),
+                           alpha: 1)
+
+        case .softAqua:
+            return UIColor(red: cgFloat(240),
+                           green: cgFloat(252),
+                           blue: cgFloat(251),
+                           alpha: 1)
+
+        case .lilac:
+            return UIColor(red: cgFloat(244),
+                           green: cgFloat(243),
                            blue: cgFloat(255),
-                           alpha: 1)
-            
-        case .red:
-            return UIColor(red: cgFloat(160),
-                           green: cgFloat(170),
-                           blue: cgFloat(230),
                            alpha: 1)
         }
     }
